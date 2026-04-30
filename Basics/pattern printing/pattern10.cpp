9308b7febe481f5b8db8d85b2f5a62a787a72f51
Abhishek <18abhishekchaudhary@gmail.com>
2026-05-01 01:22:56 +0530

    progress update

---
+++ Basics/pattern printing/pattern10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter a number : ";
+    cin >> n;
+    for (int i = 1; i<=2*n-1; i++){
+        if (i<=n){
+            for (int j = 1; j<=i; j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        else {
+            for (int k = 1; k<=2*n-i; k++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+}
--- Basics/pattern printing/pattern9.cpp
+++ Basics/pattern printing/pattern9.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
+    int x;
     cout << "enter a number : ";
-    cin >> n;
-    
+    cin >> x;
+    for (int i = 0; i <= x-1; i++){
+        for (int j = 1; j <= x-1-i; j++){
+            cout<<" ";
+        }
+        for (int k = 1; k <= 2*i+1; k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int l = 1; l<=x; l++){
+        for (int m = 2; m<=l; m++){
+            cout <<" ";
+        }
+        for(int n = 1; n<=2*(x-l)+1; n++){
+            cout <<"*";
+        }
+        cout <<endl;
+    }
 }
