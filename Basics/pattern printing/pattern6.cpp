6e27eb43ebdce1efa3d882b72ecbf43d89e4f5ce
Abhishek <18abhishekchaudhary@gmail.com>
2026-04-30 23:49:25 +0530

    progress update

---
+++ Basics/pattern printing/pattern6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout <<"enter a number : ";
+    cin>>n;
+    for (int i =0; i<=n-1; i++){
+        for (int j=1; j<=n-i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
