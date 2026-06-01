8ccc3ac2b793783e1ae4fe00882ba858ec2246c6
Abhishek <18abhishekchaudhary@gmail.com>
2026-06-01 14:02:18 +0530

    progress update

--- Arrays/Remove_Duplicates_in-place_from_Sorted_Array.cpp
+++ Arrays/Remove_Duplicates_in-place_from_Sorted_Array.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void remove_duplicate(int arr[],int n){
-    
+void remove_duplicate(vector<int>arr){
+    int n = arr.size();
+    for(int i=1;i<=n;i++){
+        if (arr[i-1]<arr[i]){
+          cout << arr[i-1]<<" ";
+        }
+    }
+
 }
 int main(){
-    
+    vector<int> arr1 ={0,0,1,1,1,2,2,3,3,4};
+    remove_duplicate(arr1);
 }
